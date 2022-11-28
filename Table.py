d76cd9bdfb7864c8887263e5f4065569fba1a646
Andrija Cenic <andrija.cenic@elfak.rs>
2022-11-28 22:48:04 +0100

    popravljeno prikazivanje kako treba, ispravke promene stanja

--- Table.py
+++ Table.py
@@ -26,21 +26,24 @@ class Table:
         for move_y in self.played_o:
             self.matrix[move_y[0]][move_y[1]] = 'O'
 
-        print(self.played_x, ' : ', self.remaining_x)
-        print(self.played_o, ' : ', self.remaining_o)
+        #print(self.played_x, ' : ', self.remaining_x)
+        #print(self.played_o, ' : ', self.remaining_o)
 
         print(' ', end=' ')
         for i in range(1, self.cols+1):
-            cprint('  ' + (chr(64+i)),'grey',attrs=['bold'] ,end=' ')  # column indices
+            cprint('  ' + (chr(64+i)), 'grey',
+                   attrs=['bold'], end=' ')  # column indices
         print()
         print(' ', end=' ')
         for j in range(0, self.cols):  # horizontal walls
             cprint('====', attrs=['bold'], end='')
         print()
-        for i in range(0, self.rows):
+        nrows = list(range(0, self.rows))
+        nrows.reverse()
+        for i in nrows:
             cprint(chr(ord('0') + i + 1) if i <
-                  10 else chr(ord('A')+i-10),'grey',attrs=['bold'] , end='')
-            cprint(' | ',attrs=['bold'], end = '')        # row index
+                   10 else chr(ord('A')+i-10), 'grey', attrs=['bold'], end='')
+            cprint(' | ', attrs=['bold'], end='')        # row index
             for j in range(0, self.cols):  # cells and vertical walls
 
                 if self.matrix[i][j] == None:
@@ -49,20 +52,22 @@ class Table:
                     if self.matrix[i][j] == 'X':
                         cprint(self.matrix[i][j], 'red', end='')
                     else:
-                         cprint(self.matrix[i][j], 'blue', end='')   
-                    cprint(' | ', attrs=['bold'],end='')     
+                        cprint(self.matrix[i][j], 'blue', end='')
+                    cprint(' | ', attrs=['bold'], end='')
 
                 if (j == self.cols-1):
-                    cprint(chr(ord('0') + i + 1),'grey',attrs=['bold'] , end='')
+                    cprint(chr(ord('0') + i + 1), 'grey',
+                           attrs=['bold'], end='')
                     print()
             print(' ', end=' ')
             for j in range(0, self.cols):  # horizontal walls
-                cprint('====',attrs=['bold'], end='')
+                cprint('====', attrs=['bold'], end='')
             print()
 
         print(' ', end=' ')
         for i in range(1, self.cols+1):
-            cprint('  ' + (chr(64+i)),'grey',attrs=['bold'] , end=' ')  # column indices
+            cprint('  ' + (chr(64+i)), 'grey',
+                   attrs=['bold'], end=' ')  # column indices
         print()
         return
 
@@ -109,6 +114,9 @@ class Table:
             self.played_x.add(move)
             self.played_x.add((move[0] + 1, move[1]))
             self.remaining_x.discard(move)
+            self.remaining_x.discard((move[0] + 1, move[1]))
+            if (move[0] == 1 or self.is_not_empty((move[0]-2, move[1]))):
+                self.remaining_x.discard((move[0]-1, move[1]))
             self.remaining_o.discard(move)
             self.remaining_o.discard((move[0], move[1] - 1))
             self.remaining_o.discard((move[0] + 1, move[1]))
@@ -117,14 +125,25 @@ class Table:
             self.played_o.add(move)
             self.played_o.add((move[0], move[1] + 1))
             self.remaining_o.discard(move)
+            self.remaining_o.discard((move[0], move[1] + 1))
+            if (move[1] == 1 or self.is_not_empty((move[0], move[1] - 2))):
+                self.remaining_o.discard((move[0], move[1] - 1))
             self.remaining_x.discard(move)
             self.remaining_x.discard((move[0] - 1, move[1]))
             self.remaining_x.discard((move[0] - 1, move[1] + 1))
             self.remaining_x.discard((move[0], move[1] + 1))
 
     def can_play(self, player) -> bool:
-        for i in range(self.rows):
-            for j in range(self.cols):
-                if (self.is_valid(player, (i, j))):
-                    return True
+        if player == 'X':
+            return len(self.remaining_x) > 0
+        if player == 'O':
+            return len(self.remaining_o) > 0
+        return False
+
+    def is_not_empty(self, move) -> bool:
+        # in range
+        if move[0] < 0 or move[1] < 0:
+            return True
+        if move in self.played_x or move in self.played_o:
+            return True
         return False
